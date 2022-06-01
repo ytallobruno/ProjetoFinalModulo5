7d05295e4e90280300592dff9af486e2b07fe9f6
Ytallo Bruno <ytallobruno@hotmail.com>
2022-06-01 17:42:30 -0300

    feat: adicionado logica para loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import { BrowserRouter, Outlet, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Main from './pages/Main/Main'
-import Carrinho from './components/Carrinho/Carrinho'
-
+import { BrowserRouter, Outlet, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Main from "./pages/Main/Main";
+import Carrinho from "./components/Carrinho/Carrinho";
+import Loader from "./components/Loader/Loader";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3500);
+  });
 
   return (
     <div className="App">
-    <BrowserRouter>
+      {isLoading == true ? (
+        <Loader />
+      ) : (
+        <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Main />}>
-              <Route path='/carrinho' element={<Carrinho />} />
+            <Route path="/" element={<Main />}>
+              <Route path="/carrinho" element={<Carrinho />} />
             </Route>
-            
           </Routes>
-    </BrowserRouter>
-  
+        </BrowserRouter>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
